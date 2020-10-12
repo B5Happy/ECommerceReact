80ccb4ff4deab25560cf87810c1aa2876fca40ec
Saly <saly@juke4me.com>
2020-10-12 16:47:09 +0200

    add sign in and sign up page also form input and custom button component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from '../src/pages/homepage/homepage.component';
-import ShopPage from '../src/pages/shop/shop.component.jsx';
-import Header from '../src/components/header/header.component.jsx';
+import ShopPage from '../src/pages/shop/shop.component';
+import SignInAndSignUpPage from '../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from '../src/components/header/header.component';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
       </div>);
 }
